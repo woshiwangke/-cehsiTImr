92a79007c945c28b009976853700ece69c1f0ef7
woshiwangke <44830150+woshiwangke@users.noreply.github.com>
2020-11-14 02:55:35 +0000

    Update ceshiJava.java

--- aaaaa/src/main/java/ceshiJava.java
+++ aaaaa/src/main/java/ceshiJava.java
@@ -2,7 +2,10 @@ public class ceshiJava {
     public static void main(String[] args) {
         System.out.println(" sout hahahahahhaa");
 
-        System.out.println(" laolihhahahaha");
+        System.out.println(" laolihhahahaha");\
+            
+            
+            System.out.println("1212121ahaha");
 }
 
 }
